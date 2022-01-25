e2b3a9b003bcf95a736bd93f630a6482bea3813d
Rob Cowart <robacj@gmail.com>
2022-01-25 12:19:36 +0100

    wip

---
+++ ml/TO-DO/codex/install_all.sh
@@ -0,0 +1,53 @@
+###################################################################################################
+# (C)Copyright 2021 ElastiFlow Inc.
+# All Rights Reserved
+# 
+# RESTRICTED RIGHTS
+# 
+# This software is supplied under the terms of the applicable license agreement established between
+# ElastiFlow Inc. and the End User.
+# 
+# Use, copying, publishing, repackaging, reselling, retransmitting, redistributing, or disclosing
+# the software is strictly prohibited unless otherwise provided in the license agreement. Any copy
+# must contain the above copyright notice and this restricted rights notice.
+# 
+# UNLESS REQUIRED BY APPLICABLE LAW OR AGREED TO IN WRITING. THE SOFTWARE IS PROVIDED "AS IS",
+# WITHOUT WARRANTY OF ANY KIND, EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+# MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT OF THIRD PARTY RIGHTS. IN
+# NO EVENT SHALL THE AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
+# CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
+###################################################################################################
+
+#!/bin/bash
+
+while getopts h:u:p: flag
+do
+  case "${flag}" in
+    h) ES_HOST=${OPTARG};;
+    u) USERNAME=${OPTARG};;
+    p) PASSWORD=${OPTARG};;
+  esac
+done
+
+if [[ "$ES_HOST" = "" ]]; then
+  ES_HOST=http://127.0.0.1:9200
+fi
+
+if [[ "$USERNAME" = "" ]]; then
+  USERNAME=elastic
+fi
+
+if [[ "$PASSWORD" = "" ]]; then
+  PASSWORD=changeme
+fi
+
+echo ""; echo "Installing all ECS-compatible anomaly_detectors and datafeeds ..."; echo ""
+./netsec_icmp_scan_direct_all_fast.sh -h ${ES_HOST} -u ${USERNAME} -p ${PASSWORD}
+./netsec_icmp_scan_direct_all_slow.sh -h ${ES_HOST} -u ${USERNAME} -p ${PASSWORD}
+./netsec_icmp_scan_direct_inbound_fast.sh -h ${ES_HOST} -u ${USERNAME} -p ${PASSWORD}
+./netsec_icmp_scan_direct_inbound_slow.sh -h ${ES_HOST} -u ${USERNAME} -p ${PASSWORD}
+./netsec_icmp_scan_direct_outbound_fast.sh -h ${ES_HOST} -u ${USERNAME} -p ${PASSWORD}
+./netsec_icmp_scan_direct_outbound_slow.sh -h ${ES_HOST} -u ${USERNAME} -p ${PASSWORD}
+./netsec_icmp_scan_direct_private_fast.sh -h ${ES_HOST} -u ${USERNAME} -p ${PASSWORD}
+./netsec_icmp_scan_direct_private_slow.sh -h ${ES_HOST} -u ${USERNAME} -p ${PASSWORD}
